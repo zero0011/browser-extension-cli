88f513075772020ef51e4b68aadbc60e02ecf74e
zsshuaizhu <zsshuaizhu@tencent.com>
2021-11-15 16:25:31 +0800

    feat:增加命令行交互

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+#!/usr/bin/env node
+
+
 "use strict";
 
-// TODO
+const path = require('path');
+const fs = require('fs-extra');
+const chalk = require('chalk');
+const spawn = require('cross-spawn');
+const commander = require('commander');
+
+const packageFile = require('./package.json');
+
+let projectName;
+
+const program = new commander.Command(packageFile.name)
+  .version(packageFile.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .action((name) => {
+    projectName = name;
+  })
+  .option('default', 'Create project with Dom')
+  .option('-react', 'Create project with react')
+  .option('-vue', 'Create project with vue')
+  .on('--help', () => {
+    console.log(`    Only ${chalk.green('<projectName>')} is required.`);
+  })
+  .parse(process.argv);
+
+
+// Exit from the process if no project name is provided
+if (typeof projectName === 'undefined') {
+  console.error('Please specify the project directory:');
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green('<projectName>')}`
+  );
+  console.log();
+  console.log('For example:');
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('my-extension')}`);
+  console.log();
+  console.log(
+    `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
+  );
+  process.exit(1);
+}
